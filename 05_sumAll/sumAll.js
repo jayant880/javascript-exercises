a8e8b4050728f781566277fa1ddeacf64be22a03
Jayant <188503548+jayant880@users.noreply.github.com>
2025-03-31 05:22:01 +0000

    completed 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (start, stop) {
+    let sum = 0;
+    if (start < 0 ||
+        stop < 0 ||
+        !Number.isInteger(start) ||
+        !Number.isInteger(stop))
+        return "ERROR";
+    if (start > stop) {
+        let temp = start;
+        start = stop;
+        stop = temp;
+    }
+    for (let integer = start; integer <= stop; integer++) {
+        sum += integer;
+    }
+    return sum;
 };
 
 // Do not edit below this line
