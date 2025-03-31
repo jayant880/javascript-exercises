4814802dc208c5ba4ae7a65f9ab8f38acfc89dfb
Jayant <188503548+jayant880@users.noreply.github.com>
2025-03-31 07:22:56 +0000

    completed 10-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (fib) {
+    let one = 1;
+    let two = 1;
+    let result;
+    fib = parseInt(fib);
+    if (fib < 0) return "OOPS";
+    if (fib == 0) return 0;
+    if (fib <= 2) return 1;
+    for (let i = 2; i < fib; i++) {
+        result = one + two;
+        one = two;
+        two = result;
+    }
+    return result
 };
 
 // Do not edit below this line
