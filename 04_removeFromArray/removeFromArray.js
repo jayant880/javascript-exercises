99e2370e13a236c575e3cf827b50d294909f59ab
Jayant <188503548+jayant880@users.noreply.github.com>
2025-03-31 04:39:43 +0000

    completed 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...value) {
+    for (let index = 0; index < array.length; index++) {
+        if (value.includes(array[index])) {
+            array.splice(index, 1);
+            index--;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
