a219a4465695b1cdedf5baf06330574fa4cffdd5
Jayant <188503548+jayant880@users.noreply.github.com>
2025-03-31 06:19:46 +0000

    completed 06-leapYear

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    // divisible by 4
+    let divBy4 = (year % 4 == 0);
+    // divisible by 100 is not leap year
+    let divBy100 = (year % 100 == 0);
+    // unless divisible by 400
+    let divBy400 = (year % 400 == 0);
+
+    if (divBy4 && (!divBy100 || divBy400)) return true;
+    return false;
 
 };
 
