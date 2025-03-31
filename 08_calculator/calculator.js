6787f3e45b3fba69257a37f999368b1f21427d4d
Jayant <188503548+jayant880@users.noreply.github.com>
2025-03-31 06:50:44 +0000

    completed 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let product = 1;
+  for (let i = 0; i < array.length; i++) {
+    product *= array[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (fact) {
+  let ans = 1;
+  while (fact) {
+    ans *= fact;
+    fact--;
+  }
+  return ans;
 };
 
 // Do not edit below this line
