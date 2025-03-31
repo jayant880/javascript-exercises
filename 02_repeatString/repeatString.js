db82e1ce5fd04edf85695e80e9c28eb47d2e52fa
Jayant <188503548+jayant880@users.noreply.github.com>
2025-03-31 02:56:16 +0000

    completed 02-repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if (times < 0) return 'ERROR';
+    let combinedString = '';
+    for(let i = 0; i<times; i++){
+        combinedString += word;
+    }
+    return combinedString;
 };
 
 // Do not edit below this line
